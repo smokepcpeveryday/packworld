3a18f5235fcff51b2b64095905563eef1de9561b
hahaAndy1 <36525452+hahaAndy1@users.noreply.github.com>
2024-08-29 20:22:57 -0400

    generate seed

--- main/packworld/spatial.gd
+++ main/packworld/spatial.gd
@@ -1,12 +1,16 @@
 extends Node3D
 
 var block = preload("res://block.tscn")
+var noise = FastNoiseLite.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for n in range(8):
-		print(n)
-		inst(Vector3(n,1,0))
+	for x in range(8):
+		for y in range(8):
+			var noise_height = noise.get_noise_1d(y)*100
+			var y_spread = round(noise_height)
+			print(noise_height)
+			inst(Vector3(y,y_spread,0))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
