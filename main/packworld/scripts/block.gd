83b0a2bd689b971f1541675196244f237df4ef04
hahaAndy1 <36525452+hahaAndy1@users.noreply.github.com>
2024-09-06 18:03:06 -0400

    implemented block breaking

--- main/packworld/scripts/block.gd
+++ main/packworld/scripts/block.gd
@@ -14,3 +14,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+#	block breaking
+	if event is InputEventMouseButton and event.is_pressed():
+		if Input.is_action_pressed("click_left"):
+			queue_free()
+	
